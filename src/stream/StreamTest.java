e9da1616b208a9aa29802422f2e5445ce1db8297
Lu.Teserat <dobry.tester.pl@gmail.com>
2024-05-17 22:58:56 +0200

    Poznawanie Setów + drobne zmiany w strumieniach

--- src/stream/StreamTest.java
+++ src/stream/StreamTest.java
@@ -2,7 +2,6 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
@@ -11,13 +10,13 @@ import java.util.stream.Stream;
 public class StreamTest {
     public static void main(String[] args) {
         // tworzenie listy, nie jest to jeszcze strumień (
-        List<String> names = Arrays.asList("Tomek", "Janek" , "Franek", "Mariusz");
+        List<String> names = Arrays.asList("Tomek", "Janek", "Franek", "Mariusz");
 
         //utworzenie strumienia z String, Tablicy, Double, Int
         Stream<String> streamNames = names.stream();
-        Stream<Integer> streamInt = Arrays.asList(new Integer[]{1,2}).stream();
-        DoubleStream doubleStream = DoubleStream.of(3,5,7,8,4,1,5,5);
-        IntStream intStream = IntStream.range(1,100);
+        Stream<Integer> streamInt = Arrays.asList(new Integer[]{1, 2}).stream();
+        DoubleStream doubleStream = DoubleStream.of(3, 5, 7, 8, 4, 1, 5, 5);
+        IntStream intStream = IntStream.range(1, 100);
 
         //Przetwarzanie danych
         //filtr - filtrowanie
@@ -35,29 +34,29 @@ public class StreamTest {
         //przekazujemy elment przy użyciu lambdy, będziemy filtrować czy imie zaczyna się od slowa T, zostaną w strumieniu tylko zaczynające się na literę T
         //potem zwiększamy rozmiat, operacja  foreach  wypisuje na ekranie element
 
-        streamNames.filter(el -> el.startsWith("T"))
-                .map(el -> el.toUpperCase() )
-                .forEach(el -> System.out.println(el));
+//        streamNames.filter(el -> el.startsWith("T"))
+//                .map(el -> el.toUpperCase() )
+//                .forEach(el -> System.out.println(el));
 
         // szukamy elementó większych od 20 po czym mnozymy przez 2, limitujemy na 10 elementów do pokazania
-
-        intStream.filter(el -> el>20)
-                .map(el -> el*2)
-                .limit(10)
-                .forEach(el -> System.out.println(el));
+//
+//        intStream.filter(el -> el>20)
+//                .map(el -> el*2)
+//                .limit(10)
+//                .forEach(el -> System.out.println(el));
 
         //Druga opcja zwrócenie wartości w long count
-        long count = intStream.filter(el -> el>20)
-                .map(el -> el*2)
-                .limit(10)
-                .count();
-
-        System.out.println(count);
+//        long count = intStream.filter(el -> el>20)
+//                .map(el -> el*2)
+//                .limit(10)
+//                .count();
+//
+//        System.out.println(count);
 
         //Przypisanie do zmiennej string
         List<String> modifiedNames =
                 streamNames.filter(el -> el.startsWith("T"))
-                        .map( el -> el.toUpperCase())
+                        .map(el -> el.toUpperCase())
                         .collect(Collectors.toList());
 
         System.out.println(modifiedNames.get(0));
